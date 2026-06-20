871aa1f74b1c2d8c32e20168c22788d025366916
paganogabriele74-ship-it <paganogabriele74@gmail.com>
2026-06-20 12:06:08 +0200

    Update sync-config.js

--- sync-config.js
+++ sync-config.js
@@ -1,5 +1,5 @@
 var SALVADANAIO_SYNC = {
-  supabaseUrl: "INCOLLA_QUI_SUPABASE_PROJECT_URL",
-  supabaseAnonKey: "INCOLLA_QUI_SUPABASE_ANON_PUBLIC_KEY",
+  supabaseUrl: "https://ugjmtvnbjbvcejvkkzjg.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnam10dm5iamJ2Y2VqdmtrempnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MjI2NDMsImV4cCI6MjA5NzQ5ODY0M30.ksRloCDLqO8iAzeDw6727vtWYnbcWdiERymlb2JEu_I",
   spaceId: "gabri-rossi-vacanza"
 };
